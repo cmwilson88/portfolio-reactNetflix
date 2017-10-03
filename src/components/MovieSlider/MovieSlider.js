a2497ad1071dab6c3068d19d207f1701fb864e67
Christopher Wilson <cmw.wilson88@gmail.com>
2017-10-03 17:54:59 -0400

    Implement React Whirligig for carousel functionality

--- src/components/MovieSlider/MovieSlider.js
+++ src/components/MovieSlider/MovieSlider.js
@@ -1,15 +1,59 @@
-import React from 'react'
-// import './movieSlider.css'
+// import React, { Component } from 'react'
+// import Slider from 'react-slick'
 
-function MovieSlider(props)  {
-    const movies = props.movies.map((movie, index) => {
-      return (
-        <div className="testMovie" key={index} style={{backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.backdrop_path})`}}></div>
-      )
-    })
+// export default class MovieSlider extends Component {
+//   render() {
+//     var settings = {
+//       dots: true,
+//       infinite: true,
+//       speed: 500,
+//       slidesToScroll: 7,
+//       slidesToShow: 7
+//     };
+
+//     const movies = this.props.movies.map((movie, index) => {
+//       return (
+//         <div className="testMovie" key={index} style={{backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`}}></div>
+//       )
+//     })
+//     return (
+//       <div>
+//         <h2>Uneven sets (infinite)</h2>
+//         <Slider {...settings}>
+//           {movies}
+//         </Slider>
+//       </div>
+//     );
+//   }
+// }
+
+
+import React from 'react'
+import Whirligig from 'react-whirligig'
+ 
+const MovieSlider = (props) => {
+  let track
+  const next = () => track.next()
+  const prev = () => track.prev()
+
+  const movies = props.movies.map((movie, index) => {
     return (
-      <div className="moviesCarousel">{movies}</div>
-    );
-  }
+      <div className="testMovie" key={index} style={{backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.backdrop_path})`}}></div>
+    )
+  })
+   
+  return (
+    <div className="movieCarousel">
+      <button onClick={prev}>Prev</button>
+      <Whirligig
+        gutter="1em"
+        ref={(_trackInstance) => { track = _trackInstance}}
+      >
+        {movies}
+      </Whirligig>
+      <button onClick={next}>Next</button>
+    </div>
+  )
+}
 
 export default MovieSlider
