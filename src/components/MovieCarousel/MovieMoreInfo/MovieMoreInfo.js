4a5d30ee96d27a2df6b7c1e06c0c45f43cb07aa7
Christopher Wilson <cmw.wilson88@gmail.com>
2017-10-09 20:34:55 -0400

    Render top 5 cast members

--- src/components/MovieCarousel/MovieMoreInfo/MovieMoreInfo.js
+++ src/components/MovieCarousel/MovieMoreInfo/MovieMoreInfo.js
@@ -6,32 +6,56 @@ class MovieMoreInfo extends Component {
 		super(props) 
 
 		this.state = {
-			detailedMovie: props.displayMovie
+			detailedMovie: props.displayMovie,
+			cast: null
 		}
 	}
 
 	componentDidMount() {
 		getMovieInfo(this.props.displayMovie.id).then(response => {
-			console.log(this.props.displayMovie.id)
-			console.log('Get movie info')
 			console.log(response)
 			this.setState({
-				detailedMovie: response
+				detailedMovie: response,
+				cast: response.credits.cast
 			})
 		}).catch(err => console.log(err))
 	}
 
 	render() {
+		const detailedMovie = this.state.detailedMovie
+		const releaseYear = detailedMovie.release_date.substr(0,4)
+		let cast;
+		if(this.state.cast) {
+			cast = this.state.cast.splice(0,5)
+							.map(castMember => {
+								return (
+									<li>{castMember.name}</li>
+								)
+							})	
+		}
 		return (
 	 		<section 
 	 			className="moreInfo" 
-	 			key={this.props.displayMovie.id} 
 	 			>
 	 			<div className="moreInfoBG"
 	 				style={{backgroundImage: `url(https://image.tmdb.org/t/p/w780${this.props.displayMovie.backdrop_path})`}}>
 	     			<div className="moreInfoOverlay">
-	     				<h1 className="hero_title">{this.props.displayMovie.title}</h1>
-	     				<p>{this.props.displayMovie.overview}</p>
+	     				<h1 className="hero_title">{detailedMovie.title}</h1>
+	     				<section className="movie_info">
+	     					<span>{detailedMovie.runtime}</span>
+	     					<span>{releaseYear}</span>
+	     				</section>
+	     				<p>{detailedMovie.overview}</p>
+	     				<br/>
+	     				<section>
+	     					<span>Cast:</span>
+	     					{cast ? (
+		     					<ul>
+		     						{cast}
+		     					</ul>
+	     					): null}
+	     				</section>
+	     				<p>{detailedMovie.tagline}</p>
 	  				</div>
 	 			</div>
 	 		</section>
--- src/services/moreInfo.js
+++ src/services/moreInfo.js
@@ -3,9 +3,5 @@ import config from '../config'
 
 export function getMovieInfo(id) {
 	return axios.get(`${config.url}/3/movie/${id}?api_key=${config.API_KEY}&append_to_response=videos,credits,recommendations`)
-			.then(response => {
-				console.log(id)
-				console.log(response.data)
-				return response.data
-			}).catch(err => console.log(err))
+			.then(response => response.data).catch(err => console.log(err))
 }
