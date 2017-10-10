1a84c22e3478e7ff318b56905ef371d3c22c1465
Christopher Wilson <cmw.wilson88@gmail.com>
2017-10-10 18:34:14 -0400

    Add directors and genres render

--- src/components/MovieCarousel/MovieMoreInfo/MovieMoreInfo.js
+++ src/components/MovieCarousel/MovieMoreInfo/MovieMoreInfo.js
@@ -7,7 +7,9 @@ class MovieMoreInfo extends Component {
 
 		this.state = {
 			detailedMovie: props.displayMovie,
-			cast: null
+			cast: null,
+			directors: null,
+			genres: null
 		}
 	}
 
@@ -16,7 +18,10 @@ class MovieMoreInfo extends Component {
 			console.log(response)
 			this.setState({
 				detailedMovie: response,
-				cast: response.credits.cast
+				cast: response.credits.cast,
+				directors: response.credits.crew
+						.filter(item => item.job === 'Director'),
+				genres: response.genres
 			})
 		}).catch(err => console.log(err))
 	}
@@ -24,15 +29,36 @@ class MovieMoreInfo extends Component {
 	render() {
 		const detailedMovie = this.state.detailedMovie
 		const releaseYear = detailedMovie.release_date.substr(0,4)
+		
 		let cast;
+		let directors;
+		let genres;
+		
 		if(this.state.cast) {
 			cast = this.state.cast.splice(0,5)
 							.map(castMember => {
 								return (
-									<li>{castMember.name}</li>
+									<li key={castMember.id}>{castMember.name}</li>
 								)
 							})	
 		}
+
+		if(this.state.directors) {
+			directors = this.state.directors.map(director => {
+				return (
+					<li key={director.id}>{director.name}</li>
+				)
+			})
+		}
+
+		if(this.state.genres) {
+			genres = this.state.genres.map(genre => {
+				return (
+					<li key={genre.id}>{genre.name}</li>
+				)
+			})
+		}
+
 		return (
 	 		<section 
 	 			className="moreInfo" 
@@ -47,14 +73,38 @@ class MovieMoreInfo extends Component {
 	     				</section>
 	     				<p>{detailedMovie.overview}</p>
 	     				<br/>
-	     				<section>
-	     					<span>Cast:</span>
-	     					{cast ? (
-		     					<ul>
-		     						{cast}
-		     					</ul>
-	     					): null}
-	     				</section>
+	     				{cast ? (
+		     				<section className="movie_info">
+		     					<span>Cast:</span>
+			     					<ul>
+			     						{cast}
+			     					</ul>
+		     				</section>
+	     				): null}
+     					{directors ? (
+	     					<section className="movie_info">
+	     						{directors.length > 1 ? (
+	     							<span>Directors:</span>
+	     						 ) : (
+	     						 	<span>Director:</span>
+	     						)} 
+	     						<ul>
+	     							{directors}
+	     						</ul>
+	     					</section>
+	     				) : null}
+	     				{genres ? (
+	     					<section className="movie_info">
+	     						{genres.length > 1 ? (
+	     							<span>Genres:</span>
+	     						) : (
+	     							<span>Genre:</span>
+	     						)}
+	     						<ul>
+	     							{genres}
+	     						</ul>
+	     					</section>
+	     				) : null}
 	     				<p>{detailedMovie.tagline}</p>
 	  				</div>
 	 			</div>
