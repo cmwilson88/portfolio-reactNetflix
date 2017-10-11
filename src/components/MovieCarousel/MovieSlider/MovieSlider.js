0f77f934f11e5b29e2316cb64ba17cd014708c74
Christopher Wilson <cmw.wilson88@gmail.com>
2017-10-11 19:37:15 -0400

    Refactor MovieTile into it's own class component.  No longer needed state in MovieSlider, therefore changed it back to functional component

--- src/components/MovieCarousel/MovieSlider/MovieSlider.js
+++ src/components/MovieCarousel/MovieSlider/MovieSlider.js
@@ -1,67 +1,40 @@
 import React, {Component} from 'react'
+import MovieTile from './MovieTile/MovieTile'
 
-class MovieSlider extends Component {
-    constructor(props) {
-      super(props)
-
-      this.state = {
-        tileActive: false
-      }
-
-      this.setActiveMovieTile = this.setActiveMovieTile.bind(this)
-    }
-
-    setActiveMovieTile() {
-      if(this.props.moreInfoActive) {
-        this.setState({
-          tileActive: false
-        })
-      } else {
-        this.setState({
-          tileActive: true
-        })
-      }
-    }
-
-    render() {
-      const props = this.props
-      const movies = props.movies.map((movie, index) => {
-        return (
-          <div 
-            onMouseEnter={() => {
-              props.mouseEnterInfo(props.movies[index])
-            }}
-            onClick={() => {
-              props.displayMoreInfo(props.movies[index])
-              this.setActiveMovieTile()
-            }}
-            className={this.state.tileActive && this.props.moreInfoMovie.id === movie.id
-                        ? 'movieTile-active' : 'movieTile'} 
-            style={{backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}}
-            key={index}>
-              <div className="tile__details">
-                <div className="tile__button">
-                    <i className="fa fa-play"></i>
-                </div>
-                <div className="tile__title">
-                  {movie.title}
-                </div>
-                <div className="tile__rating">
-                  <span
-                    style={{
-                      color: movie.vote_average >= 7 ? 'green' : movie.vote_average >= 4 ? 'orange' : 'red'
-                    }}>{movie.vote_average}</span> / 10
-                </div> 
-              </div>
-          </div>
-        )
-      })
+function MovieSlider(props){
+    const movies = props.movies.map((movie) => {
       return (
-        <div>
-            {movies}
-        </div>
-      );
-    }
+        <MovieTile
+          movie={movie} 
+          moreInfoActive={props.moreInfoActive}
+          moreInfoMovie={props.moreInfoMovie}
+          mouseEnterInfo={props.mouseEnterInfo}
+          displayMoreInfo={props.displayMoreInfo}
+          key={movie.id}/>
+      )
+    })
+    return (
+      <div>
+          {movies}
+      </div>
+    );
   }
 
 export default MovieSlider
+          // <div 
+          //   key={index}>
+          //     <div className="tile__details">
+          //       <div className="tile__button">
+          //           <i className="fa fa-play"></i>
+          //       </div>
+          //       <div className="tile__title">
+          //         {movie.title}
+          //       </div>
+          //       <div className="tile__rating">
+          //         <span
+          //           style={{
+          //             color: movie.vote_average >= 7 ? 'green' : movie.vote_average >= 4 ? 'orange' : 'red'
+          //           }}>{movie.vote_average}</span> / 10
+          //       </div> 
+          //     </div>
+          // </div>
--- src/components/MovieCarousel/MovieSlider/MovieTile/MovieTile.js
+++ src/components/MovieCarousel/MovieSlider/MovieTile/MovieTile.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function MovieTile(props) {
-	render(props) {
+class movieTile extends Component {
+	constructor(props) {
+      super(props)
+
+      this.state = {
+        tileActive: false
+      }
+
+      this.setActiveMovieTile = this.setActiveMovieTile.bind(this)
+    }
+
+    setActiveMovieTile() {
+      if(this.props.moreInfoActive) {
+        this.setState({
+          tileActive: true
+        })
+      } else {
+        this.setState({
+          tileActive: true
+        })
+      }
+    }
+    render() {
+    	const props = this.props;
 		return (
-
-		)
-	}
+		  <section  
+		  	  onMouseEnter={() => {
+		  	  	props.mouseEnterInfo(props.movie)
+		  	  	if(props.moreInfoActive) {
+		  	  		this.setActiveMovieTile()
+		  	  	}
+		  	  }}
+		  	  onClick={() => {
+		  	  	props.displayMoreInfo(props.movie)
+		  	  	this.setActiveMovieTile()
+		  	  }}
+		  	  className={this.state.tileActive && props.moreInfoMovie.id === props.movie.id
+	                        ? 'movieTile-active' : 'movieTile'} 
+	          style={{backgroundImage: `url(https://image.tmdb.org/t/p/w500${props.movie.backdrop_path}`}}>
+		      <div className="tile__details">
+		        <div className="tile__button">
+		            <i className="fa fa-play"></i>
+		        </div>
+		        <div className="tile__title">
+		          {props.movie.title}
+		        </div>
+		        <div className="tile__rating">
+		          <span
+		            style={{
+		              color: props.movie.vote_average >= 7 ? 'green' : props.movie.vote_average >= 4 ? 'orange' : 'red'
+		            }}>{props.movie.vote_average}</span> / 10
+		        </div> 
+		      </div>
+		  </section>
+		)	
+    }
 }
+
+export default movieTile
