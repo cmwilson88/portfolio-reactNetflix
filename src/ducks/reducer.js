a2c92c3fb7000044800e1027cb0803c9cf51ad65
Christopher Wilson <cmw.wilson88@gmail.com>
2017-10-21 16:50:34 -0400

    Initialize redux store

---
+++ src/ducks/reducer.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const initialState = {
+	searchMovie: [],
+	selectedMovie: null
+}
+
+
+export default function reducer(state = initialState, action) {
+	switch(action.type) {
+		default: 
+			return state;
+	}
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+
+import {Provider} from 'react-redux';
+import store from './store'
+import {BrowserRouter} from 'react-router-dom'
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+	, document.getElementById('root'));
 // registerServiceWorker();
