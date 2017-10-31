a0d56244c4eeefb4fb92306def0c934ee51d573d
Christopher Wilson <cmw.wilson88@gmail.com>
2017-10-31 09:06:44 -0400

    Add conditional rendering for video for when a video is not found on the API

--- src/components/MovieDisplay/MovieDisplay.js
+++ src/components/MovieDisplay/MovieDisplay.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {getProgramVideos} from '../../services/moreInfo'
+import './movieDisplay.css'
 
 export default class MovieDisplay extends Component {
 	constructor() {
@@ -18,25 +19,27 @@ export default class MovieDisplay extends Component {
 			console.log(response.videos.results)
 			this.setState({
 				programTitle: response.title || response.name,
-				programVideo: response.videos.results.filter(video => video.site === 'YouTube' && video.type === 'Trailer')
+				programVideo: response.videos.results.filter(video => video.site === 'YouTube' && (video.type === 'Trailer' || video.type === 'Opening Credits'))
 									.reverse()[0]
 			})
 		})
 	}
 	render() {
-		console.log(this.state)
-		console.log(this.state.programVideo.key)
-		return this.state.programVideo.key ? (
-			<div style={{width: '100vw', height: '100vh'}}>
-				<iframe 
-					title="video_player"
-					width="100%" 
-					height="100%" 
-					src={`https://www.youtube.com/embed/${this.state.programVideo.key}?autoplay=1&rel=0&showinfo=0`}
-					frameBorder="0" 
-					allowFullScreen></iframe>
+		return (
+			<div className="movie_display_body" style={{width: '100vw', height: '100vh'}}>
+				{this.state.programVideo ? (
+						<iframe 
+							title="video_player"
+							width="100%" 
+							height="100%" 
+							src={`https://www.youtube.com/embed/${this.state.programVideo.key}?autoplay=1&rel=0&showinfo=0`}
+							frameBorder="0" 
+							allowFullScreen></iframe>
+				) : (
+					<h1 className="search_header">There is currently no video for {this.state.programTitle}</h1>
+				)}
 			</div>
-		) : 'There is currently no video for ' + this.state.programTitle
+		)
 	}
 }
 
